a20457127b20f614fc7d8b2279dcee720304cd61
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-10 18:06:19 +0300

    (automated command)

--- tests/set_test_env.py
+++ tests/set_test_env.py
@@ -1,6 +1,6 @@
 import sys
 import os
 set_testEnv = os.environ['test_db'] = 'true'
-
+print(sys.path)
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
