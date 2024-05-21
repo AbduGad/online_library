99a3d151ae669cb0a6a6c8a22357824c7ec7e22f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-05-22 00:43:29 +0300

    Add new symbolic link creation logic
    Added logic to create a symbolic link from source PDF directory to destination
    static PDF directory in the web-dynamic module. This ensures proper file
    organization and access within the project.

--- Api/__init__.py
+++ Api/__init__.py
@@ -2,3 +2,4 @@ import os
 import sys
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 set_parent_dir = sys.path.append(parent_dir)
+sys.path.append(os.path.dirname(__file__))
